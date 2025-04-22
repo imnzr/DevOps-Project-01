98d3b01611f656bd81de34e925e9f367f38697c4
Nizar Fadilah <suncodev@gmail.com>
2025-04-22 22:20:24 +0700

    Add crud function

--- todo-list-api/controllers/user_controller.go
+++ todo-list-api/controllers/user_controller.go
@@ -1,24 +1,8 @@
 package controllers
 
-import (
-	"context"
-	"database/sql"
-	"fmt"
+import "net/http"
 
-	"github.com/imnzr/DevOps-Project-01/todo-list-api/models"
-)
-
-type UserRepositoryController struct {
-	DB *sql.DB
-}
-
-func (r *UserRepositoryController) Insert(ctx context.Context, user models.User) {
-	query := "INSERT INTO user(username, email, password) VALUES(?, ?, ?)"
-	result, err := r.DB.ExecContext(ctx, query)
-	if err != nil {
-		return user, fmt.Errorf("error inserting user: %w", err)
-	}
-}
-func (r *UserRepositoryController) GetById(ctx context.Context, id int64) (models.User, error) {}
-func (r *UserRepositoryController) UpdateById(ctx context.Context, user models.User)           {}
-func (r *UserRepositoryController) DeleteById(ctx context.Context, id int64) error             {}
+func Index(w http.ResponseWriter, r *http.Request)  {}
+func Add(w http.ResponseWriter, r *http.Request)    {}
+func Update(w http.ResponseWriter, r *http.Request) {}
+func Delete(w http.ResponseWriter, r *http.Request) {}
