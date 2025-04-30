a0a9127077de1fd1e0343a45e727ae4b0dac9dae
Nizar Fadilah <suncodev@gmail.com>
2025-04-30 09:28:52 +0700

    Update all repository

--- todo-list-api/controllers/user_controller.go
+++
@@ -1,8 +0,0 @@
-package controllers
-
-import "net/http"
-
-func Index(w http.ResponseWriter, r *http.Request)  {}
-func Add(w http.ResponseWriter, r *http.Request)    {}
-func Update(w http.ResponseWriter, r *http.Request) {}
-func Delete(w http.ResponseWriter, r *http.Request) {}
--- todo-list-api/main.go
+++
@@ -1,8 +0,0 @@
-package main
-
-import "github.com/imnzr/DevOps-Project-01/todo-list-api/config"
-
-func main() {
-	config.DatabaseConnection()
-
-}
