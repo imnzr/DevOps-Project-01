a5e9308b3dba83f0a891c4bdb5397a95ad4eda83
Nizar Fadilah <nizarfadilah74@gmail.com>
2025-05-13 16:13:55 +0700

    Add function on user controller

--- todo-list-api/backend/controller/implementation/user_controller_impl.go
+++ todo-list-api/backend/controller/implementation/user_controller_impl.go
@@ -1,1 +1,23 @@
 package implementation
+
+import (
+	"net/http"
+
+	"github.com/imnzr/DevOps-Project-01/todo-list-api/backend/service/service"
+	"github.com/julienschmidt/httprouter"
+)
+
+type UserControllerImpl struct {
+	UserService service.UserService
+}
+
+func (controller UserControllerImpl) Create(writter http.ResponseWriter, request *http.Request, params httprouter.Params) {
+}
+func (controller UserControllerImpl) Update(writter http.ResponseWriter, request *http.Request, params httprouter.Params) {
+}
+func (controller UserControllerImpl) Delete(writter http.ResponseWriter, request *http.Request, params httprouter.Params) {
+}
+func (controller UserControllerImpl) FindById(writter http.ResponseWriter, request *http.Request, params httprouter.Params) {
+}
+func (controller UserControllerImpl) FindByAll(writter http.ResponseWriter, request *http.Request, params httprouter.Params) {
+}
