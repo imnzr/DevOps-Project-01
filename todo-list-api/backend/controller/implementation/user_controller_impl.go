6f0178a1f721d46bd77da9384521c1b4cb52cb6a
Nizar Fadilah <nizarfadilah74@gmail.com>
2025-05-13 19:31:15 +0700

    initial commit

--- todo-list-api/backend/controller/implementation/user_controller_impl.go
+++ todo-list-api/backend/controller/implementation/user_controller_impl.go
@@ -1,8 +1,12 @@
 package implementation
 
 import (
+	"encoding/json"
+	"fmt"
 	"net/http"
 
+	web "github.com/imnzr/DevOps-Project-01/todo-list-api/backend/models/web/request"
+	"github.com/imnzr/DevOps-Project-01/todo-list-api/backend/models/web/response"
 	"github.com/imnzr/DevOps-Project-01/todo-list-api/backend/service/service"
 	"github.com/julienschmidt/httprouter"
 )
@@ -12,6 +16,26 @@ type UserControllerImpl struct {
 }
 
 func (controller UserControllerImpl) Create(writter http.ResponseWriter, request *http.Request, params httprouter.Params) {
+	decoder := json.NewDecoder(request.Body)
+	userCreateRequest := web.UserCreateRequest{}
+	err := decoder.Decode(&userCreateRequest)
+	if err != nil {
+		// error handler here
+	}
+
+	userResponse := controller.UserService.Create(request.Context(), userCreateRequest)
+	WebResponse := response.WebResponse{
+		Code:   200,
+		Status: "OK",
+		Data:   userResponse,
+	}
+
+	writter.Header().Add("Content-Type", "application/json")
+	encoder := json.NewEncoder(writter)
+	err = encoder.Encode(WebResponse)
+	if err != nil {
+		panic(fmt.Errorf("error hereL %w", err))
+	}
 }
 func (controller UserControllerImpl) Update(writter http.ResponseWriter, request *http.Request, params httprouter.Params) {
 }
---
+++ todo-list-api/backend/models/web/response/web_response.go
@@ -0,0 +1,7 @@
+package response
+
+type WebResponse struct {
+	Code   int
+	Status string
+	Data   interface{}
+}
